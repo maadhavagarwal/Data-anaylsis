810e1e2f92ee69f91358d5190ea55dead48d1862
maadhavagarwal <157884068+maadhavagarwal@users.noreply.github.com>
2024-06-22 20:23:37 +0530

    second

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import streamlit as sl
 import visulation as vis
-df= pd.read_csv("country_wise_latest.csv")
-def Drop():
+    
+def Drop(df):
    a=sl.selectbox("choose one",df.columns,index=14)
+  
    if a == "Country/Region":
       i=sl.selectbox("choose one",df["Country/Region"])
-      sl.bar_chart(df[i])
+      vis.plot_country(df,i)
    if a == "Deaths" or a == "Confirmed" or a == "Recovered":
-      vis.plot_total_cases()
-   # date_range = sl.sidebar.date_input('Select Date Range', [df['Deaths'].min(), df['Deaths'].max()])
-   # filtered_df = df[(df['Deaths'] >= date_range[0]) & (df['Deaths'] <= date_range[1])]
-   # sl.bar_chart(filtered_df['Deaths'])
+      vis.plot_total_cases(df)
+   # sl.write(df)
+   else:
+       sl.bar_chart(df[a])
+   
+   sl.write("Top 10 countries with maximum cases")
+   vis.plot_countries_with_max_cases(df.head(10))
+   sl.write("Death range selection")
+   a=sl.slider("choose death range",df["Deaths"].min(),df["Deaths"].max(),step=1000)
+   sl.write("Death range selection",a)
+   sl.bar_chart(df[df["Deaths"]>=a]["Country/Region"])
+   sl.write("New cases")
+   vis.plot_daily_cases(df)
    return a
--- visulation.py
+++ visulation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as sl
+import numpy as np
 # df = pd.read_csv('cleaned_data.csv')
 def plot_total_cases(df):
     # plt.figure(900*100)
@@ -20,18 +21,36 @@ def plot_total_cases(df):
     plt.savefig("recoverd.png")
     sl.pyplot(plt)
     # plt.savefig("")
-def plot_top_countries(df):
-     plt.scatter( df['Country/Region'])
-     sl.pyplot(plt)
-     plt.savefig("top_countries.png")
 def plot_daily_cases(df):
       plt.figure().set_figheight(35)
       plt.scatter(df['New cases'], df['Country/Region'])
       plt.savefig("daily_cases.png")
       sl.pyplot(plt)
-plot_daily_cases()
-plot_total_cases()
+# plot_daily_cases()
+# plot_total_cases()
+def plot_countries_with_max_cases(df):
+    plt.figure().set_figwidth(10)
+    plt.scatter( df['Country/Region'],df['Confirmed'])
+    plt.savefig("countries_with_max_cases.png")
+    sl.pyplot(plt)  
 
-     
+def plot_country(df,country):
+    filtered_df = df[df["Country/Region"] == country]
+    
+    if filtered_df.empty:
+        raise ValueError(f"No data found for country: {country}")
+    
+    # Create a scatter plot
+    plt.scatter(filtered_df['Country/Region'], filtered_df[filtered_df.columns[4]])
+    plt.title(f"Covid-19 Cases in {country}")
+  
+    plt.ylabel("Number of active Cases")
+    
+    # Display the plot in Streamlit
+    sl.pyplot(plt)
+    
+    # Save the plot to a file
+    plt.savefig(country + ".png")
+    
 
     
